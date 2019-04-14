f3d2b3bfd6514c23a72b72234623635c766ec491
Qun Li Huang <qunli02@gmail.com>
2019-04-14 01:19:16 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,13 +2,13 @@ def reformat_languages(languages)
   new = languages.values
   hash = new[0].merge(new[1])
   key1 = languages[:oo].keys
-  key2 = languages[:funtional].keys
+  key2 = languages[:functional].keys
   hash.each do |lan, stuff|
     if key1.include?(lan)
       hash[lan][:style] = :oo
     end
     if key2.include?(lan)
-      hash[lan][:style] = :funtional
+      hash[lan][:style] = :functional
     end
   end
 end
