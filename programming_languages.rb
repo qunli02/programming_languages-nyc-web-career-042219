3295424752201d6c367ecfb6e59bc5406761c875
Qun Li Huang <qunli02@gmail.com>
2019-04-14 01:23:08 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
-  # your code here
+  new = languages.values
+  hash = new[0].merge(new[1])
+  key1 = languages[:oo].keys
+  key2 = languages[:functional].keys
+  hash.each do |lan, stuff|
+    if key1.include?(lan) && key2.include?(lan)
+      hash[lan][:style] = [:oo, :functional]
+    elsif key1.include?(lan)
+      hash[lan][:style] = [:oo]
+    elsif key2.include?(lan)
+      hash[lan][:style] = [:functional]
+    end
+  end
 end
