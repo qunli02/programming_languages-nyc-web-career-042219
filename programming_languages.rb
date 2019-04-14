0f1a8adc4e28ccac6dfba0a32f776311c9426db6
Qun Li Huang <qunli02@gmail.com>
2019-04-14 01:08:57 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,9 +1,6 @@
 def reformat_languages(languages)
   new = languages.values
-  hash = {}
-  new.each do |x|
-    hash << x
-  end
+  hash = new[0].merge(new[1])
   length1 = languages[:oo].length
   length2 = languages[:functional].length
   new.each do |lan, type|
